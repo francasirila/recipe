8c6636ee2f091494d666301852a857f34d0cf60b
francasirila <sirilaaotieno@gmail.com>
2026-09-14 13:45:15 +0300

    removed unecessary files

--- recipe/recipe/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from models.user import User 
-from models.recipe import Recipe 
-from models.ingredient import Ingredient
-from models.category import Category
-from models.favourite import Favourite
